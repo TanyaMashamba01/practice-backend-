6e7de130257f57a617fa3c0185ab5e7660391e12
Shah Zaib <shahzaib1234@outlook.com>
2024-02-11 18:00:39 +0500

    Add cookie-parser and cors middleware to app.js

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (requestHandler) => {
+	return (res, req, next) => {
+		Promise.resolve(requestHandler(res, req, next)).catch((error) =>
+			next(error)
+		);
+	};
+};
+
+export { asyncHandler };
